60b1ecf0f3400c84b68119145ae6d4b210fe2d75
Nishant Hulwan <nishanthulwan47@gmail.com>
2020-10-24 11:42:57 -0400

    Initial commit

--- src/main/java/com/example/mvc_basics/controller/HomeController.java
+++ src/main/java/com/example/mvc_basics/controller/HomeController.java
@@ -4,14 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.time.Instant;
-
 @Controller
 public class HomeController {
 
     @RequestMapping("/home")
     public String getHomePage(Model model) {
-        model.addAttribute("welcomeMessage", Instant.now().toString());
+        model.addAttribute("greetings", new String[] {"Hi", "hello", "goodbye"});
         return "home";
     }
 
