e1df7ebbfddf7e81fee89a00ab324f5d23d8a8b3
Nishant Hulwan <nishanthulwan47@gmail.com>
2020-10-24 12:55:54 -0400

    connecting controllers with templates

---
+++ src/main/java/com/example/mvc_basics/controller/MessageForm.java
@@ -0,0 +1,14 @@
+package com.example.mvc_basics.controller;
+
+public class MessageForm {
+
+    private String text;
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
---
+++ src/main/java/com/example/mvc_basics/controller/MessageListService.java
@@ -0,0 +1,30 @@
+package com.example.mvc_basics.controller;
+
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class MessageListService {
+
+    private List<String> messages;
+
+    public MessageListService() {
+
+    }
+    @PostConstruct
+    public void postConstruct() {
+        this.messages = new ArrayList<>();
+    }
+
+    public void addMessage(String message) {
+        messages.add(message);
+    }
+
+    public List<String> getMessages() {
+        return new ArrayList<>(this.messages);
+    }
+}
+
