bad53632cb29515b830ce7004d1d2d4b0accacc7
Nishant Hulwan <nishanthulwan47@gmail.com>
2020-10-24 12:53:08 -0400

    connecting controllers

--- src/main/java/com/example/mvc_basics/controller/HomeController.java
+++ src/main/java/com/example/mvc_basics/controller/HomeController.java
@@ -2,15 +2,32 @@ package com.example.mvc_basics.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class HomeController {
 
-    @RequestMapping("/home")
-    public String getHomePage(Model model) {
-        model.addAttribute("greetings", new String[] {"Hi", "hello", "goodbye"});
+    private MessageListService messageListService;
+
+    public HomeController(MessageListService messageListService) {
+        this.messageListService = messageListService;
+    }
+
+    @GetMapping("/home")
+    public String getHomePage(@ModelAttribute("newMessage") MessageForm newMessage, Model model) {
+        model.addAttribute("greetings", this.messageListService.getMessages());
         return "home";
+    }
+
+    @PostMapping("/home")
+    public String addMessage(@ModelAttribute("newMessage") MessageForm messageform, Model model) {
+        messageListService.addMessage(messageform.getText());
+        model.addAttribute("greetings", messageListService.getMessages());
+        messageform.setText("");
+        return "home";
+
     }
 
 }
